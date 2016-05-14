05b0fa71eca52d09a3d679f1ecafa0658e5ecff2
Mustafa Taha <mustafataha93@gmail.com>
2016-05-14 23:27:11 +0300

    minor fix

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 $SCRIPT_DIR/tokenizer.perl -l en < $CORPUS.en > $CORPUS.tok.en
-$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en
 $SCRIPT_DIR/tokenizer.perl -l es < $CORPUS.es > $CORPUS.tok.es
-$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es
+nohup nice $SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en >& preprocessing.en.out &
+nohup nice $SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es >& preprocessing.es.out &
