8b31ec3d77953e1b3e9d066c0a45e80a11035f3d
Emre Oral <emre.oral@Emres-MacBook-Pro.local>
2016-05-14 15:43:40 +0300

    tokenizer added

--- preprocess.pl
+++ preprocess.pl
@@ -6,8 +6,12 @@ binmode(STDIN, ":utf8");
 binmode(STDOUT, ":utf8");
 
 LINE: while(<STDIN>) {
-    next LINE if /^\s*$/;
-
+    $_ =~ s/<[^<>]*>\s*//gm;
+    $_ =~ s/([^0-9])[.,;:¿?¡!-\/\\]+([^0-9])/$1$2/gm;
+    $_ =~ s/^[.,;:¿?¡!-\/\\]+([^0-9])/$1/gm;
+    
+    next LINE if /^\s*$/ ;
+    
     $_ =~ s/^([A-Z])/\l$1/;
-    print $_;
+    print $_ , "\n";
 }
