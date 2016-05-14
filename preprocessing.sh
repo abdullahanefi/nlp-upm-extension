545ba80e1aeff89f8f3f19f39d8df4efe003f832
Mustafa Taha <mustafataha93@gmail.com>
2016-05-14 21:24:23 +0300

    added preprocessing

---
+++ preprocessing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+$SCRIPT_DIR/tokenizer.perl -l en < $CORPUS.en > $CORPUS.tok.en
+$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en >$CORPUS.lowercased.en
+$SCRIPT_DIR/tokenizer.perl -l es < $CORPUS.es > $CORPUS.tok.es
+$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es >$CORPUS.lowercased.es
+~/srilm/bin/i686-m64/ngram-count -order 5 -interpolate -kndiscount -text $CORPUS.lowercased.es -lm $CORPUS.lm.es
+~/srilm/bin/i686-m64/ngram-count -order 5 -interpolate -kndiscount -text $CORPUS.lowercased.en -lm $CORPUS.lm.es
