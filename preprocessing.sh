b637b9ace38aac309e0c84b9e5ffd8978caac18c
Mustafa Taha <mustafataha93@gmail.com>
2016-05-15 17:20:03 +0300

    Gix language model script

--- preprocessing.sh
+++ preprocessing.sh
@@ -3,4 +3,4 @@ $SCRIPT_DIR/tokenizer.perl -l en < $CORPUS.en > $CORPUS.tok.en
 $SCRIPT_DIR/tokenizer.perl -l es < $CORPUS.es > $CORPUS.tok.es
 $SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en
 $SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es 
-nohup nice $SCRIPT_DIR/clean-corpus-n.perl $CORPUS.lowercased es en $CORPUS.clean 1 40 >& preprocessing.es.out &
+nohup nice $SCRIPT_DIR/../training/clean-corpus-n.perl $CORPUS.lowercased es en $CORPUS.clean 1 40 >& preprocessing.es.out &
