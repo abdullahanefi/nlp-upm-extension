007b10f423caad301e8059da546943996766579e
Mustafa Taha <mustafataha93@gmail.com>
2016-05-15 11:28:48 +0300

    fix tuning script

--- tuning.sh
+++ tuning.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- nohup /opt/moses/scripts/training/mert-moses.pl tuning/input tuning/reference /opt/moses/bin/moses /mnt/corpus/train/model/moses.ini --working-dir tuning --rootdir /opt/moses/bin/ >& mert.ou &
+nohup /opt/moses/scripts/training/mert-moses.pl tuning/input tuning/reference /opt/moses/bin/moses /mnt/corpus/train/model/moses.ini --working-dir tuning --rootdir /opt/moses/scripts/ >& mert.ou &
