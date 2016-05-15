37d38c36eb75693331ba5fe05b3afedc49033ddc
Mustafa Taha <mustafataha93@gmail.com>
2016-05-15 17:38:03 +0300

    update preprocessing script

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-$SCRIPT_DIR/tokenizer.perl -t -l en < $CORPUS.en > $CORPUS.tok.en
-$SCRIPT_DIR/tokenizer.perl -t -l es < $CORPUS.es > $CORPUS.tok.es
-$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en
-$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es 
-nohup nice $SCRIPT_DIR/../training/clean-corpus-n.perl $CORPUS.lowercased es en $CORPUS.clean 1 40 >& preprocessing.es.out &
+$SCRIPT_DIR/tokenizer/tokenizer.perl -t -l en < $CORPUS.en > $CORPUS.tok.en
+$SCRIPT_DIR/tokenizer/tokenizer.perl -t -l es < $CORPUS.es > $CORPUS.tok.es
+$SCRIPT_DIR/tokenizer/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en
+$SCRIPT_DIR/tokenizer/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es 
+nohup nice $SCRIPT_DIR/training/clean-corpus-n.perl $CORPUS.lowercased es en $CORPUS.clean 1 40 >& preprocessing.es.out &
