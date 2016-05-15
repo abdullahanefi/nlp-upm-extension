7e0c695344683af129dfdd38ab9ef60993c5a826
Mustafa Taha <mustafataha93@gmail.com>
2016-05-15 15:57:26 +0300

    Add cleaning to preprocessing

--- language_model.sh
+++ language_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup nice ~/srilm/bin/i686-m64/ngram-count -order 5 -interpolate -kndiscount -text $CORPUS.lowercased.es -lm $CORPUS.lm.es >& languge-model.es.out &
-nohup nice ~/srilm/bin/i686-m64/ngram-count -order 5 -interpolate -kndiscount -text $CORPUS.lowercased.en -lm $CORPUS.lm.en >& languge-model.en.out &
+nohup nice ~/srilm/bin/i686-m64/ngram-count -order 5 -interpolate -kndiscount -text $CORPUS.claen.es -lm $CORPUS.lm.es >& languge-model.es.out &
+nohup nice ~/srilm/bin/i686-m64/ngram-count -order 5 -interpolate -kndiscount -text $CORPUS.clean.en -lm $CORPUS.lm.en >& languge-model.en.out &
--- preprocessing.sh
+++ preprocessing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 $SCRIPT_DIR/tokenizer.perl -l en < $CORPUS.en > $CORPUS.tok.en
 $SCRIPT_DIR/tokenizer.perl -l es < $CORPUS.es > $CORPUS.tok.es
-nohup nice $SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en >& preprocessing.en.out &
-nohup nice $SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es >& preprocessing.es.out &
+$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.en > $CORPUS.lowercased.en
+$SCRIPT_DIR/lowercase.perl < $CORPUS.tok.es > $CORPUS.lowercased.es 
+nohup nice $SCRIPT_DIR/clean-corpus-n.perl $CORPUS.lowercased es en $CORPUS.clean 1 40 >& preprocessing.es.out &
