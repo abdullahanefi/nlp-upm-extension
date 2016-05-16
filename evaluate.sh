b21f3b08991bae8976f73699842266c9700c283c
Mustafa Taha <mustafataha93@gmail.com>
2016-05-16 21:05:09 +0300

    update evaluate script

--- evaluate.sh
+++ evaluate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup nice /opt/moses/bin/moses -f $MOSES_INI < ~/corpus/newstest2011.true.fr > ~/working/newstest2011.translated.en 2> ~/working/newstest2011.out 
-$SCRIPT_DIR/generic/multi-bleu.perl -lc ~/corpus/newstest2011.true.en < ~/working/newstest2011.translated.en
+~/workspace/mosesdecoder/bin/moses -f $MOSES_INI < /mnt/corpus/test/newstest2011.clean.es > /mnt/corpus/test/newstest2011.translated.en 2> ~/working/newstest2011.out 
+$SCRIPT_DIR/generic/multi-bleu.perl -lc /mnt/corpus/test/newstest2011.clean.es < /mnt/corpus/test/newstest2011.translated.en
